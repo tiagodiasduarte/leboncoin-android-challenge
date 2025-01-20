c8806c6959ed02edabc9a501308c3a3ae7cdb51e
Tiago Duarte <tiago.dias.duarte@gmail.com>
2025-01-20 23:13:14 +0000

    [LAC-5] Add DevicePreview to test Compose layouts across multiple screen sizes and configurations.

---
+++ app/src/main/java/com/leboncoin/challenge/ui/theme/DevicePreview.kt
@@ -0,0 +1,11 @@
+package com.leboncoin.challenge.ui.theme
+
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Devices
+
+@Retention(AnnotationRetention.SOURCE)
+@Preview(name = "Pixel 7 Pro", device = Devices.PIXEL_7_PRO)
+@Preview(name = "Pixel 4", device = Devices.PIXEL_4)
+@Preview(name = "Pixel C", device = Devices.PIXEL_C)
+@Preview(name = "Nexus 5", device = Devices.NEXUS_5)
+annotation class DevicePreviews
