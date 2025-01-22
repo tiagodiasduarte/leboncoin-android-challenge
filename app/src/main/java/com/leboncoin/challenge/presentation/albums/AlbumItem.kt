14030ebacc89b4854d27ce8fe869e34be29e53ab
Tiago Duarte <tiago.dias.duarte@gmail.com>
2025-01-22 21:07:35 +0000

    [LAC-5] Apply theme and dimensions to UI screens.

--- app/src/main/java/com/leboncoin/challenge/presentation/albums/AlbumItem.kt
+++ app/src/main/java/com/leboncoin/challenge/presentation/albums/AlbumItem.kt
@@ -4,45 +4,47 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextOverflow
 import coil3.compose.AsyncImage
 import com.leboncoin.challenge.R
 import com.leboncoin.challenge.domain.model.Album
 import com.leboncoin.challenge.ui.theme.DevicePreviews
+import com.leboncoin.challenge.ui.theme.Dimensions
 
 @Composable
 fun AlbumItem(album: Album) {
-    Column(
-        modifier = Modifier
-            .width(150.dp)
-    ) {
-        Spacer(modifier = Modifier.height(10.dp))
+    Column {
+        Spacer(modifier = Modifier.height(Dimensions.PaddingMedium))
 
         AsyncImage(
             modifier = Modifier
-                .width(150.dp)
-                .height(150.dp)
+                .width(Dimensions.AlbumItemImageSize)
+                .height(Dimensions.AlbumItemImageSize)
                 .align(Alignment.CenterHorizontally),
             model = album.url,
             error = painterResource(id = R.drawable.ic_album_placeholder),
             contentDescription = stringResource(id = R.string.albums_image_content_description),
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(Dimensions.PaddingSmall))
 
         Text(
             modifier = Modifier
-                .width(150.dp)
-                .height(100.dp)
+                .width(Dimensions.AlbumItemTitleWidth)
+                .height(Dimensions.AlbumItemTitleHeight)
                 .align(Alignment.CenterHorizontally),
             text = album.title,
-            maxLines = 3
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.bodyMedium
         )
     }
 }
